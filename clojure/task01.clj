c49634f0639473b113202de351724b9b71bf69e1
Anton Firsov <fir-anton@yandex.ru>
2021-12-03 22:01:55 +0300

    add task 01 part 2 description and clean solution

--- clojure/task01.clj
+++ clojure/task01.clj
@@ -1,18 +1,28 @@
-(def res 0)
-(def res2 0)
-(def input_vals (with-open [rdr (clojure.java.io/reader "../input/input01.txt")] (reduce conj [] (line-seq rdr))))
+(def res 0) ; result variable for task 01 part 1
+(def res2 0) ; result variable for task 01 part 2
+(def input_vals ; vector of input values
+  (mapv read-string
+        (with-open [rdr (clojure.java.io/reader "../input/input01.txt")] (reduce conj [] (line-seq rdr)))))
+
+;; Solution: Task 01 part 1
 (doseq [n (range 1 (count input_vals))]
-  (def prev (read-string (nth input_vals (- n 1))))
-  (def cur (read-string (nth input_vals n)))
+  (def prev (nth input_vals (- n 1)))
+  (def cur (nth input_vals n))
   (if (> cur prev)
     (def res (+ res 1))
-    (println "False!")))
-(println res)
-(def prev_triple_measure (+ (read-string (nth input_vals 0)) (read-string (nth input_vals 1)) (read-string (nth input_vals 2))))
+    nil))
+(println res) ; 1624
+
+;; Solution: Task 01 part 2
+(def prev_triple_measure (reduce + (take 3 input_vals)))
 (doseq [n (range 3 (count input_vals))]
-  (def cur_triple_measure (+ (read-string (nth input_vals n)) (read-string (nth input_vals (- n 1))) (read-string (nth input_vals (- n 2)))))
+  ;;(def cur_triple_measure (reduce + (drop (- n 2) (take (+ n 1) input_vals))))
+  (def cur_triple_measure (->> input_vals
+                               (take (+ n 1))
+                               (drop (- n 2))
+                               (reduce +)))
   (if (> cur_triple_measure prev_triple_measure)
     (def res2 (+ res2 1))
-    (println "False!"))
+    nil)
   (def prev_triple_measure cur_triple_measure))
-(println res2)
+(println res2) ; 1653
